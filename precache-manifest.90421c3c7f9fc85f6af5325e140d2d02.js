4b1deba43e22035c6815be79f6825575db004715
tGoh98 <cmz2@rice.edu>
2020-09-20 00:06:38 -0500

    Updates

---
+++ precache-manifest.90421c3c7f9fc85f6af5325e140d2d02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30d59cdb33ff07040a520e2f04f7999a",
+    "url": "/christinazhou99.github.io/index.html"
+  },
+  {
+    "revision": "f1b5f5ef1af6221a509e",
+    "url": "/christinazhou99.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2f60da14e52914212109",
+    "url": "/christinazhou99.github.io/static/js/2.b0722dae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/christinazhou99.github.io/static/js/2.b0722dae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1b5f5ef1af6221a509e",
+    "url": "/christinazhou99.github.io/static/js/main.4bf592fd.chunk.js"
+  },
+  {
+    "revision": "808c180dae8657c9e52b",
+    "url": "/christinazhou99.github.io/static/js/runtime-main.1494a162.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/christinazhou99.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
